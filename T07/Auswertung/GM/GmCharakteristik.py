532833089aa4bc4033ba4d66901ef36fc191af99
maoam13 <moritz-berger@t-online.de>
2018-02-28 00:00:55 +0100

    pythonsachen

--- T07/Auswertung/GM/GmCharakteristik.py
+++ T07/Auswertung/GM/GmCharakteristik.py
@@ -4,7 +4,7 @@ Created on Mon Feb 26 21:40:29 2018
 
 @author: Moritz
 """
-import Praktikum as p
+import Praktikummo as p
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
@@ -25,5 +25,48 @@ for row in csv_reader:
     x.append(wert)
 file.close()
 
-plt.figure(1)
-plt.plot(x,y)
+"""Variablen"""
+x = np.array(x)
+y = np.array(y)
+logy = np.log(y)
+plateau = 27
+xplateau = x[plateau:]
+yplateau = y[plateau:]
+UE = 316
+UG = 350
+
+
+sy = np.sqrt(x)
+slogy = 1./y * sy
+
+def f(a,x):
+    return np.log(a[0]*x+a[1])+a[2]
+
+#var,svar,chi = p.fitte_bel_function(x[11:52],logy[11:52],slogy[11:52],f,[1000000,-320,0])
+
+if 1:#logplot
+    a,sa,b,sb,chi,rest = p.lineare_regression(xplateau,np.log(yplateau),slogy[plateau:])
+    plt.figure(1)
+    ax = plt.subplot(111)
+    ax.set_xlabel("U [V]")
+    ax.set_ylabel("log(n)")
+    ax.axis([300,650,1,8])
+    ax.grid(linestyle='--')
+    plt.plot(x,logy)
+    plt.plot(x,a*x+b)
+    plt.vlines(UE,0,8,linestyle='--',color = 'r')
+    plt.vlines(UG,0,8,linestyle='--',color = 'r')
+    
+if 1:#plot
+    a,sa,b,sb,chi,rest = p.lineare_regression(xplateau,yplateau,sy[plateau:])
+    plt.figure(2)
+    ax = plt.subplot(111)
+    ax.set_xlabel("U [V]")
+    ax.set_ylabel("n")
+    ax.grid(linestyle='--')
+    ax.axis([250,650,0,1100])
+    plt.plot(x,y)
+    plt.plot(x,a*x+b)
+    plt.vlines(UE,0,1100,linestyle='--',color = 'r')
+    plt.vlines(UG,0,1100,linestyle='--',color = 'r')
+    
--- T07/Auswertung/GM/Praktikummo.py
+++ T07/Auswertung/GM/Praktikummo.py
@@ -322,4 +322,17 @@ def gewichtetes_mittel(y,ey):
 
     return (xm,sx)
 
+def fitte_bel_function_xy(x, y, ex, ey, func, startwerte):
+    """gibt wert,fehler,chi/ndof zurueck"""
+    model  = scipy.odr.Model(func)
+    data   = scipy.odr.RealData(x, y, sx=ex, sy=ey)
+    odr    = scipy.odr.ODR(data, model, beta0=startwerte)
+    output = odr.run()
+    return output.beta, output.sd_beta, output.res_var
 
+def fitte_bel_function(x, y, ey, func, startwerte):
+    model  = scipy.odr.Model(func)
+    data   = scipy.odr.RealData(x, y, sy=ey)
+    odr    = scipy.odr.ODR(data, model, beta0=startwerte)
+    output = odr.run()
+    return output.beta, output.sd_beta, output.res_var
