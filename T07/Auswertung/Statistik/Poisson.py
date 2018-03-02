47d72cbc789e398a9035f176c57a70e6b268c5e6
MCPPhysik <hansw3307@gmail.com>
2018-03-02 10:20:59 +0100

    Statistik 1. Version fertig

--- T07/Auswertung/Statistik/Poisson.py
+++ T07/Auswertung/Statistik/Poisson.py
@@ -6,6 +6,7 @@ Created on Tue Feb 27 17:28:13 2018
 """
 
 import Praktikum as p
+import scipy.special as spez
 import Peakabstaende_Funktionen as func
 import auswertung_nur_Methoden as AM
 import numpy as np
@@ -35,7 +36,7 @@ def fakultaet(n):
     return ret
 
 def poisson(A, x):
-    return A[0]**x * np.exp(-A[0]) / (x+1)
+    return A[0]**x * np.exp(-A[0]) / spez.factorial(x)
 
 #Berechne Fehler auf y
 ey = []
@@ -54,24 +55,32 @@ print sol
 
 #mittelwert und std ausrechnen und aus erwartung dann chi^2 ausrechnen
 mu = np.mean(data)
-#sig = np.std(data, ddof = 1)
-chi = AM.chiq_y(x[0:3], y[0:3], np.sqrt(y[0:3]), poisson, [mu])
+sig = np.std(data, ddof = 1)
+chi_mu = AM.chiq_y(x[0:3], y[0:3]/sum(y), ey[0:3], poisson, [mu])
+chi_sig = AM.chiq_y(x[0:3], y[0:3]/sum(y), ey[0:3], poisson, [sig**2])
+
+#x-array für plotten von fits
+x_fit = np.arange(0, 4, 0.1)
 
 #Plot
 plt.figure(1)
 plt.hist(data, bins = x, normed = True)
-plt.figtext(0.7,0.6,
+plt.figtext(0.52,0.47,
             '$\chi ^2$ - Anpassung in rot \n'
             +'Modell: P(n) = $\mu ^n / n!$ $\cdot$ $e^{-\mu}$'
             +'\n $\mu$= ('+str(np.round(sol[0][0],6))+' +/- '+str(np.round(sol[1][0],6))+') '
             +'\n $\chi ^2 / ndof$= ' + str(np.round(sol[2], 3))+' \n'
-            +'\n Erwartung aus Mittelwert und Fehler in gruen'
+            +'\n Erwartung aus Mittelwert in gruen'
             +'\n $\mu$= '+str(np.round(mu,6))
-            +'\n $\chi ^2 / ndof$= ' + str(np.round(chi, 3)))
+            +'\n $\chi ^2 / ndof$= ' + str(np.round(chi_mu, 3)) + '\n'
+            +'\n Erwartung aus Standardabweichung in gelb'
+            +'\n $\mu$= '+str(np.round(sig**2,6))
+            +'\n $\chi ^2 / ndof$= ' + str(np.round(chi_sig, 3)))
 plt.xlabel('Anzahl Pulse in 0.3s Intervallen')
 plt.ylabel('Relative Haeufigkeit')
 plt.title('Poissonverteilung der Pulsraten bei Messzeit von 0.3s')
-plt.plot(x, poisson([sol[0][0]], x), color = 'r')
-plt.plot(x, poisson([mu], x), color = 'g')
+plt.plot(x_fit, poisson([sol[0][0]], x_fit), color = 'r')
+plt.plot(x_fit, poisson([mu], x_fit), color = 'g')
+plt.plot(x_fit, poisson([sig**2], x_fit), color = 'y')
 
 print("Laufzeit: {0:9.2f} Sekunden".format(timeit.default_timer()-start_time))
