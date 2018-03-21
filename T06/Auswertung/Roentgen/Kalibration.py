a09254b5a7c98a090c2a2ae97a37abcceae47ac8
MCPPhysik <hansw3307@gmail.com>
2018-03-22 00:07:22 +0100

    Update

--- T06/Auswertung/Roentgen/Kalibration.py
+++ T06/Auswertung/Roentgen/Kalibration.py
@@ -14,12 +14,31 @@ import Methoden as m
 
 start_time=timeit.default_timer()
 
-i = 0
+i = 2
 index = ["Ag", "Cu", "I"]
 data = m.KalibrationEinlesen(i)
 data_co = m.fft_cutoff(data)
 data_x = np.arange(len(data))
 
+if i == 0:
+    startwerte1 = [3350, 40, 670, 30389]
+    startwerte2 = [3774, 28, 615, 5486]
+    peak1 = m.peak_best(np.real(data_co[0]), startwerte1)
+    peak2 = m.peak_best(np.real(data_co[0]), startwerte2)
+if i == 1:
+    startwerte1 = [1219, 16, 600, 66372]
+    startwerte2 = [1350, 16, 600, 10346]
+    peak1 = m.peak_best(np.real(data_co[0]), startwerte1)
+    peak2 = m.peak_best(np.real(data_co[0]), startwerte2)
+if i == 2:
+    startwerte1, p1 = [4329, 27, 860, 20576, 4291, 22, 12739], 2
+    startwerte2, p2 = [4885, 12, 660, 4465, 5000, 50, 1375], 2
+    #startwerte3 = [5000, 50, 660, 1375]
+    peak1 = m.peak_best(np.real(data_co[0]), startwerte1, p = p1)
+    peak2 = m.peak_best(np.real(data_co[0]), startwerte2, p = p2)
+
+#Energien der Linien in eV
+E = [[22162.9, 24942.4], [8047.8, 8905.3], [28317.2, 28612, 32294.7, 33042]]
 
 plt.figure(1)
 plt.plot(data_x, data_co[0], color = 'b')
--- T06/Auswertung/Roentgen/Methoden.py
+++ T06/Auswertung/Roentgen/Methoden.py
@@ -6,11 +6,12 @@ Created on Wed Mar 21 17:57:17 2018
 """
 
 import numpy as np
+import auswertung_nur_Methoden as AM
 
 def fft_cutoff(data):
     """returns abgeschn. Daten, orginal fft, abgeschn. fft"""
     fft = np.fft.fft(data)#fft
-    fftoriginal = fft
+    fftoriginal = np.copy(fft)
     fft[600:] = 0#cutoff bei index 600 (nur alles unter 600 verwendet)
     datacutoff = np.fft.ifft(fft)#inverse fft
     return [datacutoff,fftoriginal,fft]
@@ -19,3 +20,28 @@ def KalibrationEinlesen(i):
     index = ["Ag", "Cu", "I"]
     data = np.genfromtxt("../Daten/Roentgen/Kalibrierung_"+index[i]+"_5min.mca", delimiter = ',', skip_header = 12, skip_footer = 71)
     return data
+
+def peak_best(data, startwerte, p = 1):
+    #startwerte = [position, breite, untergrund, höhe]
+    k = startwerte[0]
+    def gauss(a,x):#einfache Gaussfunktion: Position in index 0, Höhe in 3
+        return a[2]+a[3]*np.exp(-(x-a[0])**2/(2*a[1]**2))
+    def gauss2(a,x):#doppelte Gaussfunktion: Position in index 0 und 4. Höhe in 3 und 6
+        return a[2]+a[3]*np.exp(-(x-a[0])**2/(2*a[1]**2))+a[6]*np.exp(-(x-a[4])**2/(2*a[5]**2))
+    x = np.arange(len(data))
+    sx = np.full(len(x), 1./np.sqrt(12))
+    sy = np.sqrt(data)
+    if p == 1:
+        sol1 = AM.fitte_bel_function(x[k-20:k+20], data[k-20:k+20], sx[k-20:k+20], sy[k-20:k+20], gauss, startwerte)
+        sol2 = AM.fitte_bel_function(x[k-80:k+80], data[k-80:k+80], sx[k-80:k+80], sy[k-80:k+80], gauss, startwerte)
+        chiq1, chiq2 = sol1[2], sol2[2]
+        peak, sig = (sol1[0][0] + sol2[0][0])/2, np.abs(sol1[0][0] - sol2[0][0])
+        return peak, sig, chiq1, chiq2
+    if p == 2:
+        sol1 = AM.fitte_bel_function(x[k-20:k+20], data[k-20:k+20], sx[k-20:k+20], sy[k-20:k+20], gauss2, startwerte)
+        sol2 = AM.fitte_bel_function(x[k-80:k+80], data[k-80:k+80], sx[k-80:k+80], sy[k-80:k+80], gauss2, startwerte)
+        chiq1, chiq2 = sol1[2], sol2[2]
+        peak1, sig1 = (sol1[0][0] + sol2[0][0])/2, np.abs(sol1[0][0] - sol2[0][0])
+        peak2, sig2 = (sol1[0][3] + sol2[0][3])/2, np.abs(sol1[0][3] - sol2[0][3])
+        return peak1, sig1, peak2, sig2, chiq1, chiq2
+    return 0
