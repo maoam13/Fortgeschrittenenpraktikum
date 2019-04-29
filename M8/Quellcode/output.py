95d89c0a7958883dfb6d02fb975d251efd7d3227
maoam13 <moritz-berger@t-online.de>
2019-04-30 00:42:53 +0200

    ...

--- M8/Quellcode/output.py
+++ M8/Quellcode/output.py
@@ -6,9 +6,17 @@ Created on Sat Apr 27 18:39:28 2019
 """
 import numpy as np
 data = np.genfromtxt("../Daten/output_4c3.csv",delimiter = ';')
+import matplotlib.pyplot as plt
 
-for i in range(1,5):
-    x = data[:,4*i-2]
-    y = data[:,4*i-1]
+for i in range(1,6):
+    y = data[:,4*i-2]
+    x = data[:,4*i-1]
     U = data[:,4*i-3]
     print U
+    
+    plt.figure('output')
+    plt.plot(x,y,label="$U_g = $"+str(U[0])+" V" )
+    plt.ylabel("$I_d[A]$")
+    plt.xlabel("$U_d[V]$")
+    plt.legend()
+    plt.axis([0,x[-1],0,max(y)*1.1])
