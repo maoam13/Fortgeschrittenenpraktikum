576618dde5b3b0bb54a1e466a4f23b2a39baaf55
maoam13 <moritz-berger@t-online.de>
2019-07-19 02:57:58 +0200

    part 5 and 6 complete

--- L5/Python/peaks.py
+++ L5/Python/peaks.py
@@ -8,7 +8,11 @@ Created on Tue Jul 16 12:06:26 2019
 import Praktikummo2 as p
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
 
+font = {'family' : 'normal',
+            'weight' : 'bold',
+            'size'   : 10}
 
 def near(array,value):
     idx = (np.abs(array-value)).argmin()
@@ -18,9 +22,10 @@ def lorentz(A,x):
     
 #def peaks(name):
     
-name = "2 mono bi tri flake/1.txt"
+#name = "5 full sandwich on sio2/Green laser/Pristine1.csv"
 #name = "7 Cu continuous oxidized/cont2.txt"
-data = np.genfromtxt("../Daten/"+name, delimiter = '')
+name = "6 Cu polycrystalline/poly6.txt"
+data = np.genfromtxt("../Daten/"+name, delimiter = '',skip_header = 1)
 x = data[:,0]
 y = data[:,1]
 dy = np.full(len(y),np.std(y[near(x,1750):near(x,2250)]))
@@ -33,9 +38,11 @@ b = near(x,w+d)
 w1 = x[np.argmax(y[a:b])+a]
 start = [850,10000000,w1,15000,1]
 fit = p.fitte_bel_function(x[a:b],y[a:b],dy[a:b],lorentz,start)
+fit2 = fit
 print fit[0][2],fit[1][2]
 
 draw = np.arange(w-d,w+d,0.0001)
+draw2 = draw
 yfit = lorentz(fit[0],draw)-fit[0][0]-fit[0][4]*draw
 halfmax = max(yfit)/2
 h1 = draw[near(yfit[:np.argmax(yfit)],halfmax)]
@@ -48,8 +55,8 @@ plt.plot(x,y-fit[0][0]-fit[0][4]*x)
 plt.plot(draw,lorentz(fit[0],draw)-fit[0][0]-fit[0][4]*draw)
 plt.plot([h1,h2],[halfmax,halfmax])
 
-w = 2680
-d = 120
+w = 2720
+d = 50
 a = near(x,w-d)
 b = near(x,w+d)
 w1 = x[np.argmax(y[a:b])+a]
@@ -70,5 +77,16 @@ plt.figure(2)
 plt.plot(x,y-fit[0][0]-fit[0][4]*x)
 plt.plot([h1,h2],[halfmax,halfmax])
 plt.plot(draw,lorentz(fit[0],draw)-fit[0][0]-fit[0][4]*draw)
+
+plt.figure(3)
+plt.rc('font', **font)
+plt.plot(x,y)
+plt.xlabel("Raman shift [$cm^{-1}$]",**font)
+plt.ylabel('Intensity [a.u.]', **font)
+plt.grid()
+plt.tight_layout()
+plt.plot(draw2,lorentz(fit2[0],draw2))
+plt.plot(draw,lorentz(fit[0],draw))
+
 
 #peaks("2 mono bi tri flake/1.txt")
